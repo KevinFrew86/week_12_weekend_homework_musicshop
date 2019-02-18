b8f43743d712e0a32f07384fe6f35a5bcbb72fb3
Kevin Frew <kevinfrew86@gmail.com>
2019-02-18 10:02:21 +0000

    can't get shop class finished

--- src/main/java/ShopTest.java
+++ src/main/java/ShopTest.java
@@ -1,2 +1,21 @@
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
 public class ShopTest {
+
+    Shop shop;
+
+
+    @Before
+    public void before() {
+        shop = new Shop();
+    }
+
+    @Test
+    public void canAddItemsToArrayList() {
+        shop.addItem();
+        assertEquals(1, shop.itemCount());
+    }
 }
